6c65893fdfd0780e9a73eb0af4a4621ed579d4ce
jchojnacka <33071762+jchojnacka@users.noreply.github.com>
2017-12-06 00:07:00 +0100

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        delegate int DelegateType(int x, int y);
+
+        static void Main(string[] args)
+        {
+
+            int test = 0;
+            byte[] buffer = new byte[128];
+            Console.WriteLine("begin main");
+            Task task = OperationTask(buffer);
+            Thread.Sleep(test);
+            Console.WriteLine("progress main");
+            task.Wait();
+            Console.WriteLine("end main");
+            Console.ReadKey();
+        }
+
+
+        public static async Task OperationTask(object data)
+        {
+            Console.WriteLine("Begin task");
+            await Task.Run(() =>
+                {
+                Console.WriteLine("begin async");
+                Thread.Sleep(100);
+                DelegateType foo = (x, y) => x = y;
+                foo.Invoke(20, 7);
+                Console.WriteLine("end async");
+            });
+            Console.WriteLine("end task");
+        }
+    }
+
+    
+}
